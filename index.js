0d30e017f583c828685058484c110b0762227061
vaish0904 <63866589+vaish0904@users.noreply.github.com>
2021-05-12 12:57:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var randomNumber1=Math.floor(Math.random()*6)+1;
 var randomDiceImage="Dice"+randomNumber1+".png";
-var randomImageSource="images/"+randomDiceImage;
+var randomImageSource=randomDiceImage;
 document.querySelectorAll("img")[0].setAttribute("src",randomImageSource);
 var randomNumber2=Math.floor(Math.random()*6)+1;
 var randomDiceImage1="Dice"+randomNumber2+".png";
-var randomImageSource1="images/"+randomDiceImage1;
+var randomImageSource1=randomDiceImage1;
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource1);
 //player 1 wins
 if(randomNumber1>randomNumber2){
